860ee0a6d14083dfb08bde6be34d29d22040fa9f
alisovskiy <alisovskiy@phoenixit.ru>
2020-10-02 12:19:47 +0300

    module3-task1 - fix creating constants and object

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 'use strict';
 
-const AUTHOR_AVATAR = ``;
-const OFFER_TITLE = ``;
-const OFFER_ADDRESS = `600, 350`;
-const OFFER_GUESTS = 0;
-const OFFER_DESCRIPTION = ``;
-const OFFER_PRICE = 0;
-const LOCATION_X = 0;
-const LOCATION_Y = 0;
 const OFFER_TYPE = [`palace`, `flat`, `house`, `bungalow`];
 const OFFER_ROOMS = [1, 2, 3, 4, 5];
 const OFFER_CHECKIN = [`12:00`, `13:00`, `14:00`];
@@ -25,30 +17,6 @@ const OFFER_PHOTOS = [
   `http://o0.github.io/assets/images/tokyo/hotel2.jpg`,
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
 ];
-
-const nearbyOffer = {
-  "author": {
-    "avatar": AUTHOR_AVATAR
-  },
-  "offer": {
-    "title": OFFER_TITLE,
-    "address": OFFER_ADDRESS,
-    "price": OFFER_PRICE,
-    "type": OFFER_TYPE,
-    "rooms": OFFER_ROOMS,
-    "guests": OFFER_GUESTS,
-    "checkin": OFFER_CHECKIN,
-    "checkout": OFFER_CHECKOUT,
-    "features": OFFER_FEATURES,
-    "description": OFFER_DESCRIPTION,
-    "photos": OFFER_PHOTOS
-  },
-  "location": {
-    "x": LOCATION_X,
-    "y": LOCATION_Y
-  }
-};
-
 const MIN_LOCATION_X = 100;
 const MAX_LOCATION_X = 600;
 const MIN_LOCATION_Y = 130;
@@ -58,6 +26,8 @@ const MAX_OFFER_PRICE = 10000;
 const NEARBY_OFFERS_AMOUNT = 8;
 const MAP_BUTTON_WIDTH = 50;
 const MAP_BUTTON_HEIGHT = 70;
+const MAP_BUTTON_WIDTH_GAP = MAP_BUTTON_WIDTH / 2;
+const MAP_BUTTON_HEIGHT_GAP = MAP_BUTTON_HEIGHT / 2;
 
 const randomOfNumbers = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -83,14 +53,12 @@ const addressMark = document.querySelector(`#pin`)
   .content
   .querySelector(`.map__pin`);
 
-const MAP_BUTTON_WIDTH_GAP = MAP_BUTTON_WIDTH / 2;
-const MAP_BUTTON_HEIGHT_GAP = MAP_BUTTON_HEIGHT / 2;
-
 const fragment = document.createDocumentFragment();
 
 const offersList = function () {
   const arrayForOffers = [];
   for (let i = 1; i <= NEARBY_OFFERS_AMOUNT; i += 1) {
+    const nearbyOffer = {author: {}, offer: {}, location: {}};
     nearbyOffer.author.avatar = `img/avatars/user0${i}.png`;
     nearbyOffer.location.x = randomOfNumbers(MIN_LOCATION_X, MAX_LOCATION_X);
     nearbyOffer.location.y = randomOfNumbers(MIN_LOCATION_Y, MAX_LOCATION_Y);
@@ -106,7 +74,7 @@ const offersList = function () {
     nearbyOffer.offer.description = `Отличная квартира типа ${nearbyOffer.offer.type}, цена: ${nearbyOffer.offer.price}`;
     nearbyOffer.offer.photos = elementsOfRandomArrayLength(OFFER_PHOTOS);
 
-    arrayForOffers.push(JSON.parse(JSON.stringify(nearbyOffer)));
+    arrayForOffers.push(nearbyOffer);
   }
   return arrayForOffers;
 };
