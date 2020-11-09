51d51700aed6986e9320b78c0575661ca23eb68c
alisovskiy <alisovskiy@phoenixit.ru>
2020-11-09 11:45:13 +0300

    module5-task2 - fix

--- js/uploadButton.js
+++ js/uploadButton.js
@@ -4,6 +4,6 @@ const publishForm = document.forms[`ad-form`];
 
 publishForm.addEventListener(`submit`, (evt) => {
   evt.preventDefault();
-  window.http(window.successUpload(), window.errorUpload(), new FormData(publishForm)); //todo вернуть readonly для адреса
+  window.http(window.successUpload(), window.errorUpload(), new FormData(publishForm)); // todo вернуть readonly для адреса
 
 });
