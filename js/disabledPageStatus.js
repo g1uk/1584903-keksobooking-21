c146301ac0ab26719ce154e399f7c38133ad78e9
alisovskiy <alisovskiy@phoenixit.ru>
2020-11-13 23:44:30 +0300

    master - refactoring and formatting

--- js/disabledPageStatus.js
+++ js/disabledPageStatus.js
@@ -2,11 +2,16 @@
 
 (() => {
   window.disabled = () => {
+    const MAP_BUTTON_LEFT = `570px`;
+    const MAP_BUTTON_TOP = `375px`;
     const mapFadedToken = document.querySelector(`.map`);
     const adForm = document.querySelector(`.ad-form`);
     const adFormChildrenArray = Array.from(adForm.children);
     const mapFilters = document.querySelector(`.map__filters`);
+    const mapPinMain = document.querySelector(`.map__pin--main`);
 
+    mapPinMain.style.left = MAP_BUTTON_LEFT;
+    mapPinMain.style.top = MAP_BUTTON_TOP;
     mapFadedToken.classList.add(`map--faded`);
     adForm.classList.add(`ad-form--disabled`);
     adFormChildrenArray.forEach((item) => {
@@ -15,5 +20,6 @@
     mapFilters.classList.add(`hidden`);
 
     window.marks.remove();
+    window.form.completion();
   };
 })();
